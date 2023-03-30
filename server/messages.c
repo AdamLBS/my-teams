3cc3d8fd2574c0ff80001a6413530920149de54d
Florian Grima <florian@gamercraft.com>
2023-03-30 14:08:46 +0200

    add send command

---
+++ server/messages.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** messages.c
+** File description:
+** messages
+*/
+
+#include "server.h"
+
+void send_command(void *handle, struct client *client, char *buffer)
+{
+
+}
