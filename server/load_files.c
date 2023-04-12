a714377114078e7d002c280eefdd2919896b8e88
Florian Grima <florian@gamercraft.com>
2023-04-12 17:12:27 +0200

    fix coding style

--- server/load_files.c
+++ server/load_files.c
@@ -27,19 +27,17 @@ void load_channels(struct client *cli, int nb)
     DIR *dir;dir = opendir("./channels/");struct dirent *direc;if (!dir) return;
     for (int i = 0; (direc = readdir(dir)) != NULL; i++) {
         if (strlen(direc->d_name) < 36) continue;
-        char *c_uuid = strdup(direc->d_name); c_uuid[36] = '\0';
-        char *path = malloc(sizeof(char) * ((strlen("./channels/") +
-        strlen(direc->d_name) + 1))); strcpy(path, "./channels/"); strcat(path
-        , direc->d_name); FILE *fd = fopen(path, "r");
-        if (strcmp(cli->teams[nb]->uuid, get_file_line_n(3, c_uuid
-        , "./channels/")) != 0) {
+        char *c_uuid = strdup(direc->d_name), *path; c_uuid[36] = '\0'; path =
+        malloc(sizeof(char) * ((strlen("./channels/") + strlen(direc->d_name)
+        + 1))); strcpy(path, "./channels/"); strcat(path, direc->d_name);
+        FILE *fd = fopen(path, "r"); if (strcmp(cli->teams[nb]->uuid
+        , get_file_line_n(3, c_uuid, "./channels/")) != 0) {
             fclose(fd); free(c_uuid); continue;
-        } cli->teams[nb]->channels[i] = malloc(sizeof(struct channel));
-        cli->teams[nb]->nb_channels++;
-        cli->teams[nb]->channels[i]->uuid = strdup(c_uuid);
-        cli->teams[nb]->channels[i]->name = strdup(get_file_line_n(1, c_uuid
-        , "./channels/")); cli->teams[nb]->channels[i]->desc = strdup(
-        get_file_line_n(2, c_uuid, "./channels/"));
+        } cli->teams[nb]->channels[i] = malloc(sizeof(struct channel)); cli->
+        teams[nb]->nb_channels++; cli->teams[nb]->channels[i]->uuid =
+        strdup(c_uuid); cli->teams[nb]->channels[i]->name = strdup(
+        get_file_line_n(1, c_uuid, "./channels/")); cli->teams[nb]->channels[i]
+        ->desc = strdup(get_file_line_n(2, c_uuid, "./channels/"));
         cli->teams[nb]->channels[i]->t_uuid = strdup(get_file_line_n(3, c_uuid
         , "./channels/")); cli->teams[nb]->channels[i]->nb_threads = atoi(
         get_file_line_n(4, c_uuid, "./channels/"));
@@ -77,16 +75,16 @@ void load_replies(struct client *cli, int nb, int na, int nz)
         char *path = malloc(sizeof(char) * ((strlen("./replies/") +
         strlen(direc->d_name) + 1))); strcpy(path, "./replies/"); strcat(path
         , direc->d_name); FILE *fd = fopen(path, "r");
-        if (strcmp(cli->teams[nb]->channels[na]->threads[nz]->uuid, get_file_line_n(3, r_uuid
-        , "./replies/")) != 0) {
+        if (strcmp(cli->teams[nb]->channels[na]->threads[nz]->uuid
+        , get_file_line_n(3, r_uuid , "./replies/")) != 0) {
             fclose(fd); free(r_uuid); continue;
         } struct reply *reply = get_reply_struct(cli->teams[nb], na, nz, i);
-        reply = strdup(get_file_line_n(1, r_uuid, "./replies/"));
-        reply = strdup(get_file_line_n(2, r_uuid, "./replies/"));
-        reply = strdup(get_file_line_n(3, r_uuid, "./replies/"));
-        reply = strdup(get_file_line_n(4, r_uuid, "./replies/"));
-        reply = strdup(get_file_line_n(5, r_uuid, "./replies/"));
-        reply = strdup(get_file_line_n(6, r_uuid, "./replies/"));
+        reply->tm_uuid = strdup(get_file_line_n(1, r_uuid, "./replies/"));
+        reply->c_uuid = strdup(get_file_line_n(2, r_uuid, "./replies/"));
+        reply->t_uuid = strdup(get_file_line_n(3, r_uuid, "./replies/"));
+        reply->o_uuid = strdup(get_file_line_n(4, r_uuid, "./replies/"));
+        reply->time = strdup(get_file_line_n(5, r_uuid, "./replies/"));
+        reply->msg = strdup(get_file_line_n(6, r_uuid, "./replies/"));
         free(r_uuid); fclose(fd);
     } closedir(dir);
 }
