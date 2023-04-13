24fa84ce3968f7781697094b155761c1f8d17974
Adam <adam@admlbs.fr>
2023-04-13 11:03:25 +0200

    coding style

--- client/client_commands.c
+++ client/client_commands.c
@@ -80,13 +80,13 @@ void send_commands(client_t *client)
     if (strstr(client->in_buffer, "/login"))
         login_command(client, client->in_buffer);
     else if (strlen(client->in_buffer) > 0 && client->login == 0) {
-        log_unauthorized(); memset(client->in_buffer, 0, 512); return;
+        write_log_unauthorized(); memset(client->in_buffer, 0, 512); return;
     }
     check_commands(client->in_buffer, client);
     memset(client->in_buffer, 0, MAX_BODY_LENGTH);
 }
 
-void log_unauthorized(void)
+void write_log_unauthorized(void)
 {
     client_error_unauthorized();
     return;
