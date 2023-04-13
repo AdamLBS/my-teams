51036a2d811c316ef01680f509e066cb2a6842c7
Florian Grima <florian@gamercraft.com>
2023-04-13 18:25:46 +0200

    fix an error

--- server/update_structs.c
+++ server/update_structs.c
@@ -9,7 +9,7 @@
 
 void update_thread_struct(struct thread *thr, char *t_uuid)
 {
-    thr->uuid = strdup(t_uuid); thr->replies = malloc(sizeof(reply_t *));
+    thr->uuid = strdup(t_uuid); thr->replies = malloc(sizeof(reply_t *) * 100);
     thr->o_uuid = strdup(get_file_line_n(1, t_uuid, "./threads/"));
     thr->time = strdup(get_file_line_n(2, t_uuid, "./threads/"));
     thr->name = strdup(get_file_line_n(3, t_uuid, "./threads/"));
